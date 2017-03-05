f6393c64891bb82ebca783af7e389c64b4832b1c
rocker231341 <relax231342@gmail.com>
2017-03-05 16:18:17 +0800

    add home

---
+++ RecordsBook/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecordsBook.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Homt
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
